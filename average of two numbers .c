2163a7d0909733df94ba3b9649a7e36928be4ac9
vinay007k <thevinay576@gmail.com>
2025-06-28 08:56:55 +0530

    Create average of two numbers .c

---
+++ average of two numbers .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   int a,b;
+   printf("enter a numbers a:");
+   scanf("%d",&a);
+   printf("enter a numbers b:");
+    scanf("%d",&b);
+   if(a>b){
+       printf("a is greatest");
+   } else{
+       printf("b is greatest");
+   }
+    return 0;
+}
